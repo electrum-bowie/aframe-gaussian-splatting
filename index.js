9770cd76264227ba5462ea423665f296db921a23
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-14 20:54:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -158,20 +158,9 @@ AFRAME.registerComponent("gaussian_splatting", {
 				covAndColorTexture.needsUpdate = true;
 
 				let splatIndexArray = new Uint32Array(vertexCount);
-				const visibleSplatIndexes = new Uint32Array(splatIndexArray.length); // Create an array to store visible splats
-				let visibleSplatCount = 0;
+				const splatIndexes = new THREE.InstancedBufferAttribute(splatIndexArray, 1, false);
+				splatIndexes.setUsage(THREE.DynamicDrawUsage);
 
-				for (let i = 0; i < splatIndexArray.length; i++) {
-					const opacity = covAndColorData_uint8[i * 4 + 3] / 255.0;
-					if (opacity > 0.55) { // Modify this condition based on your desired opacity threshold
-						visibleSplatIndexes[visibleSplatCount] = splatIndexArray[i];
-						visibleSplatCount++;
-					}
-				}
-
-				// Create a new buffer attribute for the visible splats
-				const visibleSplatIndexesAttr = new THREE.InstancedBufferAttribute(visibleSplatIndexes.slice(0, visibleSplatCount), 1, false);
-				visibleSplatIndexesAttr.setUsage(THREE.DynamicDrawUsage);
 				const baseGeometry = new THREE.BufferGeometry();
 				const positionsArray = new Float32Array(6 * 3);
 				const positions = new THREE.BufferAttribute(positionsArray, 3);
@@ -185,7 +174,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 				positions.needsUpdate = true;
 
 				const geometry = new THREE.InstancedBufferGeometry().copy(baseGeometry);
-				geometry.setAttribute('splatIndex', visibleSplatIndexesAttr);
+				geometry.setAttribute('splatIndex', splatIndexes);
 				geometry.instanceCount = vertexCount;
 
 				const material = new THREE.ShaderMaterial({
