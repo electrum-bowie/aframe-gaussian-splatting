3ff07811c733525284a0a677311c264f0d0eed8c
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-14 23:57:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 				f_buffer[8 * i + 3 + 2]
 			);
 			// Check if the scale is smaller than the threshold
-			if (Math.max(scale.x, scale.y, scale.z) < 0.055) {
+			if (Math.max(scale.x, scale.y, scale.z) < 0.057) {
 				// Skip processing this splat
 				continue;
 			}
