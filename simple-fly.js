9da285c73f05ce19a842be57fb6cbbe7e7f887cc
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-15 15:01:38 +0300

    s

--- simple-fly.js
+++ simple-fly.js
@@ -3,32 +3,33 @@ AFRAME.registerComponent('simple-fly', {
         speed: { type: 'number', default: 0.1 }
     },
     init: function () {
-        this.rightController = null;
-        this.isFlying = false;
-
-        const controllers = document.querySelectorAll('a-entity[oculus-touch-controls]');
-        if (controllers.length >= 2) {
-            this.rightController = controllers[0]; // Use 0 for the right hand
-            this.rightController.addEventListener('triggerdown', () => {
-                this.startFlying();
-                console.log('Flying started');
-            });
-            this.rightController.addEventListener('triggerup', () => {
-                this.stopFlying();
-                console.log('Flying stopped');
-            });
-        } else {
-            console.warn('No Oculus Touch controllers found.');
-        }
-
-        this.gaussianSplattingEntity = document.querySelector('[gaussian_splatting]');
-        if (!this.gaussianSplattingEntity) {
-            console.warn('No Gaussian Splatting entity found.');
-        }
+        
     },
     tick: function () {
         if (this.isFlying) {
             this.fly();
+        }
+        this.gaussianSplattingEntity = document.querySelector('[gaussian_splatting]');
+        if (!this.gaussianSplattingEntity) {
+            console.warn('No Gaussian Splatting entity found.');
+            this.rightController = null;
+            this.isFlying = false;
+
+            const controllers = document.querySelectorAll('a-entity[oculus-touch-controls]');
+            if (controllers.length >= 2) {
+                this.rightController = controllers[0]; // Use 0 for the right hand
+                this.rightController.addEventListener('triggerdown', () => {
+                    this.startFlying();
+                    console.log('Flying started');
+                });
+                this.rightController.addEventListener('triggerup', () => {
+                    this.stopFlying();
+                    console.log('Flying stopped');
+                });
+            } else {
+                console.warn('No Oculus Touch controllers found.');
+            }
+
         }
     },
     startFlying: function () {
