bc4b45f0a000b19c0f5a85692a2bedadbb16fc2c
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-15 13:50:24 +0300

    s

---
+++ input.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fileInput = document.getElementById("fileInput");
+
+    fileInput.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        const url = URL.createObjectURL(file);
+
+        // Create an A-Frame entity
+        const entity = document.createElement("a-entity");
+        entity.setAttribute("gaussian_splatting", `gltf-model: #model;`);
+        entity.setAttribute("rotation", "0 0 0");
+        entity.setAttribute("position", "0 1.5 -2");
+
+        // Create an A-Frame assets item with the URL of the uploaded file
+        const assets = document.querySelector("a-assets");
+        const assetItem = document.createElement("a-asset-item");
+        assetItem.setAttribute("id", "model");
+        assetItem.setAttribute("src", url);
+        assets.appendChild(assetItem);
+
+        // Append the entity to the scene
+        document.getElementById("entityContainer").appendChild(entity);
+
+        // Hide the file input
+        fileInput.style.display = "none";
+    });
+});
