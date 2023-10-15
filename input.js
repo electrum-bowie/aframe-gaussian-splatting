f105466d5177594b2639d7b78a0fb767ec1b55c9
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-15 15:15:44 +0300

    Update input.js

--- input.js
+++ input.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             entity.setAttribute("position", "0 1.5 -2");
 
             // Append the entity to the scene
-            document.getElementById("a-scene").appendChild(entity);
+            document.querySelector("a-scene").appendChild(entity);
 
             // Hide the file input
             fileButton.style.display = "none";
