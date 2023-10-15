808e3ef973f336655011fc995b668c23bd497466
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-15 17:05:33 +0300

    Update input.js

--- input.js
+++ input.js
@@ -1,31 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     const fileButton = document.getElementById("fileButton");
     const fileInput = document.getElementById("fileInput");
+    const scene = document.querySelector("a-scene");
 
     fileButton.addEventListener("click", () => {
         fileInput.click();
     });
+
+    // Check if a URL is stored in local storage
+    const storedURL = localStorage.getItem("plyFileURL");
+    if (storedURL) {
+        createAndAppendEntity(storedURL);
+    }
 
     fileInput.addEventListener("change", (event) => {
         const file = event.target.files[0];
         if (file) {
             const url = URL.createObjectURL(file);
 
-            // Create an A-Frame entity
-            const entity = document.createElement("a-entity");
-            entity.setAttribute("gaussian_splatting", `src: ${url};`);
-            entity.setAttribute("rotation", `0 0 0`);
-            entity.setAttribute("position", `0 1.5 -2`);
+            // Store the URL in local storage
+            localStorage.setItem("plyFileURL", url);
 
-            // Append the entity to the scene
-            document.querySelector("a-scene").appendChild(entity);
+            // Create and append the entity
+            createAndAppendEntity(url);
 
             // Hide the file input
             fileButton.style.display = "none";
             // Reload the page after a short delay
             setTimeout(() => {
                 location.reload();
-            }, 1000); // Reload after 1 second (adjust the delay as needed)
+            }, 1000);
         }
     });
+
+    // Function to create and append the entity
+    function createAndAppendEntity(url) {
+        const entity = document.createElement("a-entity");
+        entity.setAttribute("gaussian_splatting", `src: ${url};`);
+        entity.setAttribute("rotation", "0 0 0");
+        entity.setAttribute("position", "0 1.5 -2");
+        scene.appendChild(entity);
+    }
 });
