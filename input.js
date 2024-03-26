daadfdd105edf68eb6ec1f088952812e3ad60065
Stoian Andre <theendergaming26@gmail.com>
2024-03-26 15:31:40 +0200

    Update input.js

--- input.js
+++ input.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const fileButton = document.getElementById("fileButton");
     const fileInput = document.getElementById("fileInput");
+
     fileButton.addEventListener("click", () => {
         fileInput.click();
     });
+
     fileInput.addEventListener("change", (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -28,17 +30,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
                                     // Create an A-Frame entity
                                     const entity = document.createElement("a-entity");
-                                    document.addEventListener("DOMContentLoaded", function () {
+                                    entity.setAttribute("gaussian_splatting", `src: ${url};`);
+                                    entity.setAttribute("rotation", `0 0 0`);
+                                    entity.setAttribute("position", `0 1.5 -2`);
 
-                                        entity.setAttribute("gaussian_splatting", `src: ${url};`);
-                                        entity.setAttribute("rotation", `0 0 0`);
-                                        entity.setAttribute("position", `0 1.5 -2`);
-                                        // Append the entity to the scene
-                                        document.querySelector("a-scene").appendChild(entity);
-                                        // Hide the file input
-                                        fileButton.style.display = "none";
-                                    });
-                                }
+                                    // Append the entity to the scene
+                                    document.querySelector("a-scene").appendChild(entity);
+
+                                    // Hide the file input
+                                    fileButton.style.display = "none";
+                                });
+                            }
                         });
                     } catch (error) {
                         console.error("Error loading ZIP file:", error);
