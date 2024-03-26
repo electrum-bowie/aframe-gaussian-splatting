09279e700cd9eaeee3e59f65114c9e685bf8e19d
Stoian Andre <theendergaming26@gmail.com>
2024-03-26 15:37:16 +0200

    Update input.js

--- input.js
+++ input.js
@@ -1,55 +1,41 @@
+async function readExportedPly(file) {
+    const zipper = new JSZip();
+    const unzippedFiles = await zipper.loadAsync(file);
+
+    // Iterate through the unzipped files
+    for (const fileName in unzippedFiles.files) {
+        const unzippedFile = unzippedFiles.files[fileName];
+        console.warn("Please Wait. (Estimated 60s)");
+
+        // Generate a Blob for the .ply file
+        const blob = await unzippedFile.async("blob");
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+        return url;
+    }
+    return Promise.reject('No .ply file was found');
+}
 document.addEventListener("DOMContentLoaded", function () {
     const fileButton = document.getElementById("fileButton");
     const fileInput = document.getElementById("fileInput");
-
     fileButton.addEventListener("click", () => {
         fileInput.click();
     });
-
-    fileInput.addEventListener("change", (event) => {
+    fileInput.addEventListener("change", async (event) => {
         const file = event.target.files[0];
         if (file) {
-            // Check if the selected file is a .zip file
-            if (file.name.endsWith('.zip')) {
-                const reader = new FileReader();
-
-                reader.onload = async function (event) {
-                    const zip = new JSZip();
-                    const zipData = event.target.result;
-
-                    try {
-                        const zipArchive = await zip.loadAsync(zipData);
-
-                        // Iterate through the files in the ZIP archive
-                        zipArchive.forEach((relativePath, zipEntry) => {
-                            if (!zipEntry.dir) {
-                                // Assuming you want to use the first non-directory file found
-                                const fileData = zipEntry.async("blob");
-                                fileData.then(function (content) {
-                                    const url = URL.createObjectURL(content);
-
-                                    // Create an A-Frame entity
-                                    const entity = document.createElement("a-entity");
-                                    entity.setAttribute("gaussian_splatting", `src: ${url};`);
-                                    entity.setAttribute("rotation", `0 0 0`);
-                                    entity.setAttribute("position", `0 1.5 -2`);
-
-                                    // Append the entity to the scene
-                                    document.querySelector("a-scene").appendChild(entity);
-
-                                    // Hide the file input
-                                    fileButton.style.display = "none";
-                                });
-                            }
-                        });
-                    } catch (error) {
-                        console.error("Error loading ZIP file:", error);
-                    }
-                };
-
-                reader.readAsArrayBuffer(file);
-            } else {
-                alert("Please select a .zip file.");
+            try {
+                const url = await readExportedPly(file);
+                // Create an A-Frame entity
+                const entity = document.createElement("a-entity");
+                entity.setAttribute("gaussian_splatting", `src: ${url};`);
+                entity.setAttribute("rotation", `0 0 0`);
+                entity.setAttribute("position", `0 1.5 -2`);
+                document.querySelector("a-scene").appendChild(entity);
+                fileButton.style.display = "none";
+            } catch (error) {
+                alert("Error: " + error);
             }
         }
     });
