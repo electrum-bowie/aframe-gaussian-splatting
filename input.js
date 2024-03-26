df9b5f1af4bdffa86775a66f92749f80ab8a8fd9
Stoian Andre <theendergaming26@gmail.com>
2024-03-26 19:30:02 +0200

    Update input.js

--- input.js
+++ input.js
@@ -8,25 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     fileInput.addEventListener("change", (event) => {
         const file = event.target.files[0];
-        const url = URL.createObjectURL(file);
+        if (file) {
+            // Create an A-Frame entity
+            const entity = document.createElement("a-entity");
+            entity.setAttribute("gaussian_splatting", `src: ${file};`);
+            entity.setAttribute("rotation", `0 0 0`);
+            entity.setAttribute("position", `0 1.5 -2`);
 
-        // Create an A-Frame entity
-        const entity = document.createElement("a-entity");
-        entity.setAttribute("gaussian_splatting", `src: #model;`);
-        entity.setAttribute("rotation", "0 0 0");
-        entity.setAttribute("position", "0 1.5 -2");
+            // Append the entity to the scene
+            document.querySelector("a-scene").appendChild(entity);
 
-        // Create an A-Frame assets item with the URL of the uploaded file
-        const assets = document.querySelector("a-assets");
-        const assetItem = document.createElement("a-asset-item");
-        assetItem.setAttribute("id", "model");
-        assetItem.setAttribute("src", url);
-        assets.appendChild(assetItem);
-
-        // Append the entity to the scene
-        document.getElementById("entityContainer").appendChild(entity);
-
-        // Hide the file input
-        fileButton.style.display = "none";
+            // Hide the file input
+            fileButton.style.display = "none";
+        }
     });
 });
