721d8d6392aa71c3d8df8c297cb03e556f44cf2d
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2024-03-31 06:08:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -319,7 +319,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 				f_buffer[8 * i + 3 + 2]
 			);
 			// Check if the scale is smaller than the threshold
-			if (Math.max(scale.x, scale.y, scale.z) > 3) {
+			if (Math.max(scale.x, scale.y, scale.z) > 2) {
 				// Skip processing this splat
 				continue;
 			}
